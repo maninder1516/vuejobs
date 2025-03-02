912beaf2735f5860ca7a8abb8a6226b8245d5aba
Maninder Kumar <maninder.kumar@izmoltd.com>
2025-03-02 21:43:46 +0530

    Initial Commit for Vue JS 3 Project

---
+++ src/App2.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return {
+      name : 'Maninder',
+      status: true,
+      tasks: ['Task1', 'Task2', 'Task3', 'Task4', 'Task5'],
+      link: 'https://www.google.com',
+    }
+  },
+  methods: {
+    changeStatus() {
+      this.status = !this.status;
+    }
+  }
+};
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status">User is active</p>
+  <ul>
+    <li v-for="task in tasks" :key="task"> {{ task }} </li>
+  </ul>
+  <!-- <a v-bind:href="link">Google</a> -->
+  <a :href="link">Google</a>
+  <!-- <button v-on:click="changeStatus">Change Status</button> -->
+  <button @click="changeStatus">Change Status</button>
+</template>
+
+<style scoped>
+  /* h1 {
+    color: blue;
+  } */
+</style>
