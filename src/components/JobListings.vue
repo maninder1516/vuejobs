329bcd649fa8fbdf743a0e93b004795b2b02a88b
Maninder Kumar <maninder.kumar@izmoltd.com>
2025-03-03 21:33:24 +0530

    Job Listing App is completed

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -3,11 +3,18 @@
 import { RouterLink } from 'vue-router';
 import JobListing from './JobListing.vue';
 //import jobData from '@/jobs.json';
-import { ref, defineProps, onMounted } from 'vue';
+//import { ref, defineProps, onMounted } from 'vue';
+import { reactive, defineProps, onMounted } from 'vue';
 import axios from 'axios';
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue';
 
 //const jobs = ref(jobData);
-const jobs = ref([]);
+//const jobs = ref([]);
+const state = reactive({
+    jobs : [],
+    isLoading : true
+});
+
 
 defineProps({
     limit: Number,
@@ -19,10 +26,14 @@ defineProps({
 
 onMounted(async () => {
     try {
-        const response = await axios.get('http://localhost:5000/jobs');
-        jobs.value = response.data;
+        const response = await axios.get('/api/jobs');
+        //jobs.value = response.data;
+
+        state.jobs = response.data;
     } catch(error){
         console.error('Error fetching the Jobs', error);
+    } finally {
+        state.isLoading = false;
     }
 });
 </script>
@@ -34,11 +45,22 @@ onMounted(async () => {
             <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">
             Browse Jobs
             </h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" 
+            <!-- Show Loading Splinner while loading is true  -->
+            <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+                <PulseLoader />
+            </div>
+
+            <!-- Show job listing when done  -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" 
                     :key="job.id" 
                     :job="job" 
                 />
+
+                <!-- <JobListing v-for="job in jobs.slice(0, limit || jobs.length)" 
+                    :key="job.id" 
+                    :job="job" 
+                /> -->
 
                 <!-- <div v-for="job in jobs" :key="job.id" class="bg-white rounded-lg shadow-md p-4">
                     {{ job.title }}
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '@/views/HomeView.vue';
 import JobsView from '@/views/JobsView.vue';
 import NotFoundView from '@/views/NotFoundView.vue';
 import JobView from '@/views/JobView.vue';
+import AddJobView from '@/views/AddJobView.vue';
+import EditJobView from '@/views/EditJobView.vue';
 
 const routes = [
     {
@@ -20,10 +22,16 @@ const routes = [
         name: 'job',
         component: JobView,
     },
-
-    
-      
-
+    {
+        path: '/jobs/add',
+        name: 'add-job',
+        component: AddJobView,
+    },
+    {
+        path: '/jobs/edit/:id',
+        name: 'edit-job',
+        component: EditJobView,
+    },      
     {
         path: '/:catchAll(.*)',
         name: 'NotFound',
